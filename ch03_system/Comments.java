b5a6c1cc3fd2c97db5465089ccee857487ca52b6
pc11 <dalpoong3@gmail.com>
2023-09-22 10:16:48 +0900

    d

---
+++ ch03_system/Comments.java
@@ -0,0 +1,52 @@
+package ch03_system;
+
+public class Comments {
+
+    // 자바 문서(javadoc) 주석
+    // 메소드
+
+
+    /**
+     * 좋아하는 과일
+     *
+     */
+    static String apple = "사과";
+
+    public static void main(String[] args) {
+
+        // 주석 (comment)
+
+        // 단일 주석 (코드 앞에 //)
+        // 프로그래밍 언어마다 주석이 다른 경우가 많다
+        // 단축키 [Ctrl + /]
+
+        // 영영 선택후 단축키를 입력하면 한번에 주석 처리/해제
+//        System.out.println("주석");
+//        System.out.println("주석");
+//        System.out.println("주석");
+//        System.out.println("주석");
+
+        /* 다중 주석 */
+        /*
+        * 작성자: 조강연
+        * 작성일 : 2020-09-20
+        * */
+
+        System.out.println(apple);
+
+        // TODO 주석
+        // 개발 중에 구현해야할 부분에 대해서
+        // 미리 코멘트를 남겨놓는 경우 이용한다.
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
