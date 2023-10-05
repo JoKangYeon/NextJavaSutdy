93cf170d367b8c9398a2ce0e3093ddf43f3f515d
pc11 <dalpoong3@gmail.com>
2023-10-06 08:56:05 +0900

    내용 추가

--- submit05/Submit05.java
+++ submit05/Submit05.java
@@ -15,31 +15,27 @@ public class Submit05 {
         String myBinaryStr = makeBinary(23);
         System.out.println(myBinaryStr);
 
+        makeTriangle(5);
+        makeTriangle(7);
+        makeTriangle(26);
 
 
+    } // 메소드 끝
 
+    public static void makeTree(int num) {
 
-
-
-
-
-
-
-    }
-    public static void makeTree(int num){
-
-        for(int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++) {
             // i=0 공백이 4칸 *은 1개
             // i=1 공백이 3칸 *은 3개
             // i=2 공백이 2칸 *은 5개  (i에 2를 곱한 후 1을 더함)
 
             String blank = "";
-            for(int k = 0; k < num - i; k++){
+            for (int k = 0; k < num - i; k++) {
                 blank += " ";
             }
 
             String stars = "";
-            for (int j = 0; j  < (i*2)+1 ; j++){
+            for (int j = 0; j < (i * 2) + 1; j++) {
                 stars += "*";
 
             }
@@ -47,32 +43,68 @@ public class Submit05 {
         }
     }
 
-    public static String reverseStr(String word){
+    public static String reverseStr(String word) {
         String subword = "";
-        for(int i = word.length(); i > 0; i = i-1){
+        for (int i = word.length(); i > 0; i = i - 1) {
             subword += word.substring(i - 1, i);
         }
-        return  subword;
+        return subword;
     }
 
     public static String makeBinary(int num) {
         String word = ""; // 빈 문자열 선언
+        String reverseWord = ""; // 거꾸로 빈 문자열
         // num을 2진수로 바꾸기
 
-        while(true){
-
+        while (true) {
             word = word + (num % 2);  //나머지를 word에 더하기
             num = num / 2;
-            if(num == 1){
+            if (num == 1) {
                 break;
             }
-            String reverseWord = "";
-        for(int i = word.length(); i > 0; i = i-1){
-            reverseWord += word.substring(i - 1, i);
-            }
-
         }
-//        return"1" + reverseWord;
+        for (int i = 1; i <= word.length(); i++) {
+            reverseWord += word.substring(word.length() - i, (word.length() + 1) - i);
+        }
+
+        return "1" + reverseWord;
+    }
+
+    public static void makeTriangle(int num) {
+        // 공백 트리
+        //     *
+        //    * *
+        //   *   *
+        //  *     *
+        // *********
+
+        num -= 1; // 트리높이 - 맨밑줄 (1)
+
+        for (int i = 0; i < num; i++) {
+            // 공백 출력
+            for (int k = 0; k < num - i; k++) {
+                System.out.print(" ");
+            }
+            // 왼쪽 별 출력
+            for (int t = 0; t < 1; t++) {
+                System.out.print("*");
+            }
+            // 공백 출력
+            for (int j = 0; j < (2 * i) - 1; j++) {
+                System.out.print(" ");
+            }
+            // 오른쪽 별 출력
+            for (int t = 0; t < 1; t++) {
+                if (i > 0 && i < num) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= 2 * num; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
 
