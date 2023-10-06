fa3661d95dde48db84c47d49e088d7e90aec36d2
pc11 <dalpoong3@gmail.com>
2023-10-06 18:26:08 +0900

    내용 추가

--- submit06/Submit06.java
+++ submit06/Submit06.java
@@ -7,14 +7,17 @@ public class Submit06 {
         String[] nameList = {"이정재", "김혜수", "이병헌", "송중기", "유아인", "류준열", "공유", "마동석", "이종석",
                 "황정민", "박소담", "이선균", "이성민", "최민식", "이동휘", "권상우", "소지섭", "공효진", "조정석", "허성태", "이하늬"};
 
-        for(int i = 0; i < nameList.length; i++){
-            for(int k = 0; k < 3; k++){
-
-                if(nameList[i].substring(k, k + 1){
-
+        int count = 0; // 이씨 성인 배우 수
+        for (int i = 0; i < nameList.length; i++){
+            for (int k = 0; k < 1; k++) {
+                if ((nameList[i].substring(k, k + 1).equals("이"))){
+                    count += 1;
                 }
             }
         }
+        System.out.println("이씨 성을 가진 배우가 층 " + count + "명 있습니다.");
+
+
 
 
 
