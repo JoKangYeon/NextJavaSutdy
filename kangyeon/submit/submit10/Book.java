5b588f771181d9bc66035fe8edc3a1c0313174d7
pc11 <dalpoong3@gmail.com>
2023-10-18 10:19:33 +0900

    내용 추가

--- kangyeon/submit/submit10/Book.java
+++ kangyeon/submit/submit10/Book.java
@@ -1,9 +1,9 @@
 package kangyeon.submit.submit10;
 
 public class Book {
-    private int no;          /* 도서 번호 */
-    private String title;       /* 도서 제목 */
-    private boolean borrow;      /* 대여 여부 */
+    /*private int no;          *//* 도서 번호 *//*
+    private String title;       *//* 도서 제목 *//*
+    private boolean borrow;      *//* 대여 여부 *//*
 
     public Book() {
     }
@@ -51,6 +51,60 @@ public class Book {
     public void setFalse(boolean aFalse) {
         borrow = aFalse;
     }
+*/
 
+    private int no;          /* 도서 번호 */
+    private String title;       /* 도서 제목 */
+    private boolean isBorrow;      /* 대여 여부 */
 
+    public Book() {
+    }
+
+    public Book(int no, String title, boolean isBorrow) {
+        this.no = no;
+        this.title = title;
+        this.isBorrow = isBorrow;
+    }
+
+    @Override
+    public String toString() {
+
+        String state = "[" +
+                "책번호: " + no +
+                ", 책 제목: " + title +
+                ", 대여상태: " + "입고중" +
+                ']';
+        if(isBorrow){
+            state = "[" +
+                    "책번호: " + no +
+                    ", 책 제목: " + title +
+                    ", 대여상태: " + "입고중" +
+                    ']';
+        }
+       return state;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isBorrow() {
+        return isBorrow;
+    }
+
+    public void setBorrow(boolean borrow) {
+        isBorrow = borrow;
+    }
 }
--- kangyeon/submit/submit10/LibraryMain.java
+++ kangyeon/submit/submit10/LibraryMain.java
@@ -7,7 +7,7 @@ public class LibraryMain {
 
     public static void main(String[] args) {
 
-        String strBooks = "삼국지, 해리포터와 마법사의 돌, 해리포터와 비밀의 방 , 해리포터와 아즈카반의 죄수,해리포터와 불사조 기사단, 해리포터와 혼혈왕자," +
+     /*   String strBooks = "삼국지, 해리포터와 마법사의 돌, 해리포터와 비밀의 방 , 해리포터와 아즈카반의 죄수,해리포터와 불사조 기사단, 해리포터와 혼혈왕자," +
                 " 해리포터와 죽음의 성물,어린왕자, 나의 라임 오렌지나무, 이것이 자바다, " +
                 "좋은생각, 반지의 제왕: 반지 원정대, 반지의 제왕: 두 개의 탑, 반지의 제왕: 왕의 귀환, 토익보카, 개미";
 
@@ -36,9 +36,86 @@ public class LibraryMain {
 
         // 스캐너 객체 소환
         Scanner scan = new Scanner(System.in);
+        // 대여 행동을 위한 변수 선언
+        int count = 0;
 
-        while(true){
+        while (true) {
+            System.out.println("행동을 선택해주세요.");
+            System.out.println("1. 책 입고 | 2. 책 대여 | 3. 책 목록 | 4. 책 검색 | 5. 종료");
+            System.out.print(">>> ");
 
+            int command = Integer.parseInt(scan.nextLine());
+
+            if (command == 1) {
+                // 책 입고
+                System.out.println("도서 번호를 입력해주세요.");
+                System.out.print(">>> ");
+                int inputNo = Integer.parseInt(scan.nextLine());
+                if(bookList.getBookList().size() < inputNo){
+                    System.out.println("존재하지 않는 번호입니다.");
+                    continue;
+                }
+                bookList.inBook(inputNo);
+            } else if (command == 2) {
+                // 책 대여
+                if (count == 0) {
+                    System.out.println("도서 번호를 입력해주세요.");
+                    System.out.print(">>> ");
+                    int inputNum = Integer.parseInt(scan.nextLine());
+                    if(bookList.getBookList().size() < inputNum){
+                        System.out.println("존재하지 않는 번호입니다.");
+                        continue;
+                    }
+                    bookList.outBook(inputNum);
+                } else if (count == 1) {
+                    count--;
+                    System.out.println("도서 제목을 입력해주세요.");
+                    System.out.print(">>> ");
+                    String inputTitle = scan.nextLine();
+                    bookList.searchOneBook(inputTitle);
+                } else if (count > 1) {
+                    count = 0;
+                    System.out.println("도서 제목을 입력해주세요.");
+                    System.out.print(">>> ");
+                    String inputTitle = scan.nextLine();
+                    bookList.searchBook(inputTitle);
+
+                    System.out.println("도서 번호를 입력해주세요.");
+                    System.out.print(">>> ");
+                    int inputNum = Integer.parseInt(scan.nextLine());
+                    // 해리포터가 아닌 다른도서 번호를 입력했을 때.......
+                    bookList.outBook(inputNum);
+                }
+            } else if (command == 3) {
+                // 책 목록
+                bookList.checkBook();
+
+            } else if (command == 4) {
+                // 책 검색
+                System.out.println("도서 제목을 입력해주세요.");
+                System.out.print(">>> ");
+                String inputTitle = scan.nextLine();
+                for (int i = 0; i < bookList.getBookList().size(); i++) {
+                    if (bookList.getBookList().get(i).getTitle().contains(inputTitle)) {
+                        System.out.println(bookList.getBookList().get(i));
+                        count++;
+                    }
+                }
+
+
+            } else if (command == 5) {
+                // 종료
+                System.out.println("잠시 후 종료됩니다.");
+                break;
+            }
+
+        }*/
+
+        Library lib =Library.getInstance();
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
             System.out.println("행동을 선택해주세요.");
             System.out.println("1. 책 입고 | 2. 책 대여 | 3. 책 목록 | 4. 책 검색 | 5. 종료");
             System.out.print(">>> ");
@@ -49,58 +126,46 @@ public class LibraryMain {
                 // 책 입고
                 System.out.println("도서 번호를 입력해주세요.");
                 System.out.print(">>> ");
-                int inputNo = Integer.parseInt(scan.nextLine());
-                bookList.inBook(inputNo);
-                System.out.println(bookList.getBookList().get(inputNo - 1).getTitle() + " 이(가) 입고되었습니다.");
-            }else if(command == 2){
+                int no = Integer.parseInt(scan.nextLine());
+                lib.back(no);
+
+            }else if (command == 2){
                 // 책 대여
                 System.out.println("도서 번호를 입력해주세요.");
                 System.out.print(">>> ");
-                int inputNum = Integer.parseInt(scan.nextLine());
-                bookList.outBook(inputNum);
-                System.out.println(bookList.getBookList().get(inputNum - 1).getTitle() + " 을 대여하셨습니다.");
+                int no = Integer.parseInt(scan.nextLine());
+                lib.borrow(no);
+
+
             }else if(command == 3){
                 // 책 목록
-                bookList.checkBook();
+                lib.printBook();
 
             }else if(command == 4){
                 // 책 검색
-                System.out.println("도서 이름을 입력해주세요.");
+                System.out.println("도서 제목을 입력해주세요.");
                 System.out.print(">>> ");
-                String inputTitle = scan.nextLine();
-                int count = 0;
-                for(int i = 0; i < bookList.getBookList().size(); i++){
-                    if(bookList.getBookList().get(i).getTitle().contains(inputTitle)){
-                        count++;
-                    }
-                }
-                if(count == 1){
-                    for(int i = 0; i < bookList.getBookList().size(); i++){
-                        if(bookList.getBookList().get(i).getTitle().contains(inputTitle)){
-                            bookList.getBookList().get(i).setFalse(true);
-                            System.out.println(bookList.getBookList().get(i).getTitle() + "을 대여하셨습니다.");
-                        }
-                    }
-                }else if(count >= 1){
-                    for(int i = 0; i < bookList.getBookList().size(); i++){
-                        if(bookList.getBookList().get(i).getTitle().contains(inputTitle)){
-                            System.out.println(bookList.getBookList().get(i));
-                        }
-                    }
-                    System.out.println("도서 번호를 입력해주세요.");
-                    System.out.print(">>> ");
-                    int inputNum = Integer.parseInt(scan.nextLine());
-                    bookList.getBookList().get(inputNum - 1).setFalse(true);
-                    System.out.println(bookList.getBookList().get(inputNum - 1).getTitle() + "을 대여하셨습니다.");
-                }
 
-            }else if(command == 5){
+                String keyword = scan.nextLine();
+
+                lib.searchBook(keyword);
+
+            }else{
                 // 종료
-                System.out.println("잠시 후 종료됩니다.");
+                System.out.println("종료되었습니다.");
                 break;
             }
 
+
+
         }
+
+
+
+
+
+
+
 
 
 
