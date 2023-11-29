0a64351cc5573652cf1f06d314010816e12b1ec9
JoKangYeon <145143819+JoKangYeon@users.noreply.github.com>
2023-11-29 20:51:55 +0900

    Add files via upload

---
+++ MakeShop/shoppingBagJS.js
@@ -0,0 +1,16 @@
+let selectedList = JSON.parse(localStorage.getItem("shoppingList"))
+
+
+
+
+
+
+
+
+
+
+// 선택된 상품들 토탈 가격
+for (let i = 0; i < selectedList.length; i++) {
+    // if(document.querySelector("")
+
+}
